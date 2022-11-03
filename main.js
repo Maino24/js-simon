c440a6e12bbd054af35df362bd9dce76664670da
Maino24 <mg02.gabriele@gmail.com>
2022-11-03 18:03:14 +0100

    inserisco numeri random nell'array

--- main.js
+++ main.js
@@ -6,5 +6,8 @@ for ( i = 0; i < 5; i++){
     
     //genero numero random
     numeroRandom = Math.floor(Math.random()* 100 ) + 1;
-
+    //inserisco numeri random generati nell'array
+    array.push(numeroRandom)
 }
+
+console.log(array)
